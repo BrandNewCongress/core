f5134a3017e7eb2b385363564933e511a43e8468
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2017-07-03 23:25:39 -0500

    change target of prod mailer config

--- config/prod.exs
+++ config/prod.exs
@@ -26,7 +26,7 @@ config :core, [nb_slug: "brandnewcampaign", nb_token: System.get_env("NB_TOKEN")
 config :logger, level: :info
 
 # Use Mailgun
-config :core, Core.EventTemplate,
+config :core, Core.Mailer,
   adapter: Swoosh.Adapters.Mailgun,
   api_key: System.get_env("MAILGUN_KEY"),
   domain: System.get_env("MAILGUN_DOMAIN")
