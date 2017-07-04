c4790fc6a88e10748ba42c085b03f79596cd4932
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2017-07-04 00:35:35 -0500

    works! improvements include: ajaxifying form submits, subpage rerenders

---
+++ lib/core/turbo_vdom_plug.ex
@@ -0,0 +1,11 @@
+defmodule Core.TurboVdomPlug do
+  def init(default), do: default
+
+  def call(%Plug.Conn{params: %{"empty" => "true"}} = conn, _default) do
+    Phoenix.Controller.put_layout conn, {Core.LayoutView, "empty.html"}
+  end
+
+  def call(conn, _default) do
+    conn
+  end
+end
--- web/router.ex
+++ web/router.ex
@@ -7,6 +7,7 @@ defmodule Core.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Core.TurboVdomPlug, []
   end
 
   pipeline :api do
@@ -17,7 +18,7 @@ defmodule Core.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
-    get "/platform", PageController, :platform
+    # get "/platform", PageController, :platform
     # get "/candidates", PageController, :candidates
     # get "/candidates/:candidate", PageController, :candidate
 
