f3feeae23743ef20c90d30bbeae804efcce2af68
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2017-07-06 11:43:08 -0400

    notify of no need for update url

--- web/controllers/update_controller.ex
+++ web/controllers/update_controller.ex
@@ -4,17 +4,7 @@ defmodule Core.UpdateController do
 
   require Logger
 
-  def cosmic(conn, %{"secret" => @secret}) do
-    Cosmic.update()
-    json conn, %{"ok" => "Successfully updated Cosmic cache"}
-  end
-
-  def cosmic(conn, %{"secret" => s}) do
-    Logger.error "User put incorrect secret #{s}. Should be #{@secret}"
-    json conn, %{"error" => "Wrong secret. Contact Ben for the right one."}
-  end
-
-  def cosmic(conn, _) do
-    json conn, %{"error" => "Missing secret. Contact Ben for it."}
+  def cosmic(conn, params) do
+    json conn, %{"unnecessary" => "Ben implemented webhooks! No need to visit hit this link any more. If it's not updating, contact Ben. Thanks!"}
   end
 end
