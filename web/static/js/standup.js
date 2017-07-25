b1b1b53ee70742c61a0538b32c79cc7d8a0318a1
saikat <saikat@gomockingbird.com>
2017-07-25 16:07:58 +0100

    Ugly divs to set up basic layout. Need CSS.

--- web/static/js/standup.js
+++ web/static/js/standup.js
@@ -19,6 +19,9 @@ import store from './lib/standup-store'
  */
 
 class Standup extends Component {
+  styles = {
+
+  }
   state = {
     videos: []
   }
@@ -32,10 +35,17 @@ class Standup extends Component {
 
   render() {
     const { videos } = this.state
-
     return (
       <div>
-        Hello, world!
+        <div>[COSMIC DATA]Video</div>
+        <div>
+          <div>Button for people</div>
+          <div>Logo</div>
+          <div>Button for people running</div>          
+        </div>
+        <div>
+          [COSMIC DATA]Because of your pressure, 43 members of Congress decided to co-sponsor Conyers' house bill for Medicare for All. But as the health care debate rages, many of these same co-sponsors have publicly been advocating for half-measures like increasing subsidies for insurance companies or simply playing defense against Medicaid cuts. These congresspeople are silently backing Medicare for All just as a weak political move to cover their bases. We believe it's time for them to grow a spine and stand up for Medicare for All. We're calling on sitting members of Congress and anyone running for Congress to go on video pledging to stand up for Medicare for All in any public appearances and statements addressing our country's healthcare crisis.
+        </div>        
         {videos.map(({ district, first_name, email, rep, link }) =>
           <div>
             {`${first_name} in ${district} wants ${rep} to support medicare for all!`}
@@ -43,6 +53,23 @@ class Standup extends Component {
             Watch their endorsement <a href={link}> here </a>
           </div>
         )}
+        <hr />
+        <div>
+          <div>
+            [COSMIC DATA - LIST OF VIDEOS]
+          </div>
+          <div>
+            [TWEET STREAM]
+          </div>          
+        </div>
+        <hr />
+        <div>
+          [COSMIC DATA - STATS]
+        </div>
+        <hr />
+        <div>
+          [BEN'S COMPONENT]
+        </div>
       </div>
     )
   }
