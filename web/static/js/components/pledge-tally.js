7b9f99d6f5bb5bd7e8992dbc50f31e1a7cf4fde1
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2017-07-25 18:49:26 -0400

    stacked heads

--- web/static/js/components/pledge-tally.js
+++ web/static/js/components/pledge-tally.js
@@ -21,35 +21,74 @@ export default class PledgeTally extends Component {
     return (
       <div
         style={{
-          display: 'flex'
+          display: 'flex',
+          flexDirection: 'column'
         }}
       >
-
-        {Object.keys(congress).sort().map(state =>
-          <div
-            key={state}
-            style={{
-              display: 'flex',
-              flexDirection: 'column'
-            }}
-          >
-            {state}
-            {congress[state].map(rep =>
+        <div style={{ display: 'flex', marginTop: '35px' }}>
+          {Object.keys(congress).sort().map(state =>
+            <div
+              style={{
+                width: '15px',
+                height: '50px',
+                fontSize: '8px',
+                marginLeft: '2px',
+                marginRight: '2px'
+              }}
+            >
               <div
-                key={rep.name}
-                className="grayscale"
                 style={{
-                  backgroundImage: `url(${rep.img})`,
-                  backgroundSize: 'cover',
-                  height: '50px',
-                  width: '50px'
+                  transform: 'rotate(270deg) translate(20px, -41px)',
+                  width: '100px'
                 }}
               >
+                {state}
               </div>
-            )}
+            </div>
+          )}
+        </div>
 
-          </div>
-        )}
+        <div
+          style={{
+            backgroundColor: 'grey',
+            paddingTop: '1px',
+            paddingBottom: '1px',
+            marginBottom: '15px'
+          }}
+        />
+
+        <div
+          style={{
+            display: 'flex'
+          }}
+        >
+          {Object.keys(congress).sort().map(state =>
+            <div
+              key={state}
+              style={{
+                display: 'flex',
+                flexDirection: 'column'
+              }}
+            >
+              {congress[state].map(rep =>
+                <div
+                  className="grayscale"
+                  key={rep.name}
+                  style={{
+                    backgroundImage: `url(${rep.img})`,
+                    backgroundSize: 'cover',
+                    height: '15px',
+                    width: '15px',
+                    marginTop: '1px',
+                    marginBottom: '1px',
+                    marginLeft: '2px',
+                    marginRight: '2px'
+                  }}
+                />
+              )}
+            </div>
+          )}
+        </div>
       </div>
     )
   }
