a060d8d4957d8f60de7109e295e1c7ee6d788da2
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2017-07-27 15:47:43 -0400

    districts to ets

---
+++ lib/clients/district/parser.ex
@@ -0,0 +1,22 @@
+defmodule District.Parser do
+  def load_geojsons do
+    {:ok, districts} =
+      "./lib/clients/district/geojsons"
+      |> File.ls()
+
+    geojsons =
+      districts
+      |> Enum.map(fn district ->
+        {:ok, file} = "./lib/clients/district/geojsons/#{district}" |> File.read()
+        {:ok, %{"geometry" => geometry}} = file |> Poison.decode()
+
+        geometry
+        |> Geo.JSON.decode()
+       end)
+
+    districts
+    |> Enum.map(fn str -> str |> String.split(".") |> List.first() end)
+    |> Enum.zip(geojsons)
+    |> Enum.into(%{})
+  end
+end
