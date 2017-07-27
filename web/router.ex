1c52a7ab0afa0250fd3f76f2dfd2f2fb98334e9e
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2017-07-27 10:31:07 -0400

    standup routes

--- web/router.ex
+++ web/router.ex
@@ -17,39 +17,39 @@ defmodule Core.Router do
   scope "/", Core do
     pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
+    # get "/", PageController, :index
     # get "/platform", PageController, :platform
     # get "/candidates", PageController, :candidates
     # get "/candidates/:candidate", PageController, :candidate
-    # get "/standup", StandupController, :get
-
-    get "/petition/:petition", PetitionController, :get
-    post "/petition/:petition", PetitionController, :post
-
-    get "/form/:form", FormController, :get
-    get "/info/:info", InfoController, :get
-
-    get "/act", ActController, :get
-    post "/act", ActController, :post # used for setting the district
-
-    get "/act/call", ActController, :get_call
-    get "/act/call/:candidate", ActController, :get_candidate_call
-    get "/act/:candidate", ActController, :legacy_redirect
-
-    get "/events", EventsController, :get
-    get "/events/:slug", EventsController, :get_one
-    post "/events/:slug", EventsController, :rsvp
-
-    get "/call", VoxController, :get
-    get "/call/logins", VoxController, :get_logins
-    post "/call", VoxController, :post
-
-    get "/unsubscribe", SubscriptionController, :unsubscribe_get
-    post "/unsubscribe", SubscriptionController, :unsubscribe_post
-    get "/unsubscribe/:candidate", SubscriptionController, :unsubscribe_candidate_get
-    post "/unsubscribe/:candidate", SubscriptionController, :unsubscribe_candidate_post
-
-    get "/entry", EntryController, :get
+    get "/", StandupController, :get
+    
+    # get "/petition/:petition", PetitionController, :get
+    # post "/petition/:petition", PetitionController, :post
+    #
+    # get "/form/:form", FormController, :get
+    # get "/info/:info", InfoController, :get
+    #
+    # get "/act", ActController, :get
+    # post "/act", ActController, :post # used for setting the district
+    #
+    # get "/act/call", ActController, :get_call
+    # get "/act/call/:candidate", ActController, :get_candidate_call
+    # get "/act/:candidate", ActController, :legacy_redirect
+    #
+    # get "/events", EventsController, :get
+    # get "/events/:slug", EventsController, :get_one
+    # post "/events/:slug", EventsController, :rsvp
+    #
+    # get "/call", VoxController, :get
+    # get "/call/logins", VoxController, :get_logins
+    # post "/call", VoxController, :post
+    #
+    # get "/unsubscribe", SubscriptionController, :unsubscribe_get
+    # post "/unsubscribe", SubscriptionController, :unsubscribe_post
+    # get "/unsubscribe/:candidate", SubscriptionController, :unsubscribe_candidate_get
+    # post "/unsubscribe/:candidate", SubscriptionController, :unsubscribe_candidate_post
+    #
+    # get "/entry", EntryController, :get
   end
 
 
