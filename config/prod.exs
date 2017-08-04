92771582cfafbbd10962181f65f4414ae5233785
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2017-08-04 17:35:19 -0400

    change channel origin

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ config :core, Core.Endpoint,
   http: [port: {:system, "PORT"}],
   server: true,
   url: [scheme: "https", host: "m4a.gigalixirapp.com", port: 443],
-  check_origin: ["https://now.brandnewcongress.org", "https://now.justicedemocrats.com"],
+  check_origin: ["https://standup4medicare.com", "https://m4a.gigalixirapp.com"],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: "${SECRET_KEY_BASE}"
