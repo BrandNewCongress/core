8dfa8d246791fd9410304718698c4d2dd6fdbfef
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2017-08-04 17:21:38 -0400

    hardcode app name

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ use Mix.Config
 config :core, Core.Endpoint,
   http: [port: {:system, "PORT"}],
   server: true,
-  url: [scheme: "https", host: "${APP_NAME}.gigalixirapp.com", port: 443],
+  url: [scheme: "https", host: "m4a.gigalixirapp.com", port: 443],
   check_origin: ["https://now.brandnewcongress.org", "https://now.justicedemocrats.com"],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
