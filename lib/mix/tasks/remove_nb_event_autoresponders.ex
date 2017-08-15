48960e25b3bdca9d42d37e75c895e882b363b34e
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2017-08-15 14:32:28 -0400

    time for those templates

---
+++ lib/mix/tasks/remove_nb_event_autoresponders.ex
@@ -0,0 +1,33 @@
+defmodule Mix.Tasks.RemoveNbEventAutoReponders do
+  use Mix.Task
+
+  def run(_) do
+    Nb.Events.stream_all()
+    |> Stream.filter(&has_autoresponder/1)
+    |> Stream.map(&remove_autoresponder/1)
+    # |> Stream.map(&write/1)
+    |> Enum.to_list()
+    |> length()
+    |> IO.inspect
+  end
+
+  defp has_autoresponder(%{"autoresponse" => %{"broadcaster_id" => broadcaster_id, "subject" => subject, "body" => body}}) do
+    broadcaster_id != nil and subject != nil and body != nil
+  end
+
+  defp has_autoresponder(_) do
+    false
+  end
+
+  defp remove_autoresponder(event) do
+    Map.put(event, "autoresponse", %{"broadcaster_id" => nil, "subject" => nil, "body" => nil})
+  end
+
+  defp time_zone_preserve_tuple({event, lat_lng}) do
+    {event, Maps.time_zone_of(lat_lng)}
+  end
+
+  defp write(new_event = %{"id" => id}) do
+    Nb.Events.update(id, new_event)
+  end
+end
--- web/controllers/events_controller.ex
+++ web/controllers/events_controller.ex
@@ -41,10 +41,13 @@ defmodule Core.EventsController do
 
     banner = get_banner(event.type)
 
-    Nb.Events.Rsvps.create(event.id,
-      %{"first_name" => first_name, "last_name" => last_name, "email" => email,
-        "phone" => phone, "primary_address" => %{"address1" => address1, "zip" => zip,
-        "city" => city, "state" => state}})
+    primary_address = %{"address1" => address1, "zip" => zip, "city" => city, "state" => state}
+
+    Nb.Events.Rsvps.create(event.id, ~m{first_name, last_name, email, phone, primary_address})
+
+    Task.async(fn ->
+      Core.Mailer.on_rsvp(event, ~m{first_name, last_name, email})
+    end)
 
     render conn, "rsvp.html", [event: event, person: true, title: event.title, description: event.description, banner: banner] ++ GlobalOpts.get(conn, params)
   end
