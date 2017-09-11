efabf68b6348aab9928b0b226c6416e5413dc2be
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2017-09-11 13:25:16 -0400

    try prod secret

---
+++ config/prod.secret.exs
@@ -0,0 +1,10 @@
+use Mix.Config
+
+config :osdi, Osdi.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  database: System.get_env("RDS_DB_NAME"),
+  username: System.get_env("RDS_DB_USER"),
+  password: System.get_env("RDS_DB_PASSWORD"),
+  hostname: System.get_env("RDS_DB_HOST"),
+  port: "5432",
+  types: GeoExample.PostgresTypes
