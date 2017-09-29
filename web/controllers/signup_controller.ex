e41bd517d487058713ed4f9f488e844aa7473487
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2017-09-28 22:25:39 -0500

    remove inspects

--- web/controllers/signup_controller.ex
+++ web/controllers/signup_controller.ex
@@ -2,23 +2,13 @@ defmodule Core.SignupController do
   use Core.Web, :controller
 
   def simple(conn, signup_body = %{"person" => %{}}) do
-    IO.inspect signup_body
-
     response =
       signup_body
       |> to_atom_map()
-      |> IO.inspect()
       |> Osdi.PersonSignup.main()
       |> do_remove_tags(signup_body)
 
-    # LEFT OFF - Osdi.PersonSignup.main isn't adding associations
-
     json conn, response
-  end
-
-  def simple(conn, mistake_body) do
-    IO.inspect Map.keys(mistake_body)
-    json conn, %{"woops" => "woops"}
   end
 
   defp do_remove_tags(person, %{"remove_tags" => tags_to_remove}), do:
