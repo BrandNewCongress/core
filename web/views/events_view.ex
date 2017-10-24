f29862d1e43d0a2c51e2eb9a61f83b5d9800e4f2
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2017-10-24 00:22:07 -0700

    improvmeents to rsvp conversion

--- web/views/events_view.ex
+++ web/views/events_view.ex
@@ -1,3 +1,26 @@
 defmodule Core.EventsView do
   use Core.Web, :view
+
+  def candidate_of(tags) do
+    tags
+    |> Enum.reject(& String.contains?(&1, "Justice Democrats") or String.contains?(&1, "Brand New Congress"))
+    |> Enum.filter(& String.contains?(&1, "Calendar"))
+    |> List.first()
+  end
+
+  def get_donate_url(nil), do: nil
+  def get_donate_url(candidate) do
+    IO.inspect candidate
+    %{"metadata" => %{"donate_url" => donate_url}} =
+      candidate
+      |> String.split(":")
+      |> List.last()
+      |> String.trim()
+      |> String.downcase()
+      |> String.replace(" ", "-")
+      |> IO.inspect
+      |> Cosmic.get()
+
+    donate_url
+  end
 end
